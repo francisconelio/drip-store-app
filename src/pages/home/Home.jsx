cf784cc72ba2c5e163e4ae7b38ebd654749ac81a
Francisco Nélio <nelio.romero.fn@gmail.com>
2023-09-04 22:00:20 -0300

    inicio do home

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MenuBar from '../../components/header/MenuBar'
+import Footer from '../../components/footer/Footer'
+
+const Home = () => {
+  return (
+    <div>
+        <MenuBar/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
